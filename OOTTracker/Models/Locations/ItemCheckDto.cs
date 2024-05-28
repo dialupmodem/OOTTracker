2c2cc26ffc9452dcd42f1c7d9f69340c3457e7d5
dialupmodem <18317934+dialupmodem@users.noreply.github.com>
2024-05-28 00:49:48 -0400

    location/item check quick edit

---
+++ OOTTracker/Models/Locations/ItemCheckDto.cs
@@ -0,0 +1,10 @@
+﻿namespace OOTTracker.Models.Locations
+{
+    public class ItemCheckDto
+    {
+        public Guid ItemCheckId { get; set; }
+        public string? CheckType { get; set; }
+        public string? Description { get; set; }
+        public string? AgeRequirement { get; set; }
+    }
+}
