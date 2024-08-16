77f1cfa64c27d1b79ad7dbb458d73361aabfbd0c
dialupmodem <18317934+dialupmodem@users.noreply.github.com>
2024-08-16 19:43:49 -0400

    update menu, add global taghelper import, redirect home to playthroughs

--- OOTTracker/Controllers/HomeController.cs
+++ OOTTracker/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace OOTTracker.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Index", "Playthroughs");
         }
     }
 }
